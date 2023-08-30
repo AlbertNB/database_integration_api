0451f4d281c7b19e78040959080364d26ae070a2
Alberto <albertonetobrocardo@gmail.com>
2023-08-30 19:39:41 -0300

    database manager updated

--- src/database.py
+++ src/database.py
@@ -3,10 +3,10 @@ import mysql.connector
 class DBManager():
     def __init__(self, host, user, password, database):
         self.conn = mysql.connector.connect(
-            self.host=host
-            self.user=user
-            self.password=password
-            self.database=database
+            host=host,
+            user=user,
+            password=password,
+            database=database,
           )
         
         self.cursor = self.conn.cursor()
@@ -16,19 +16,25 @@ class DBManager():
         try:
             self.cursor.executemany("INSERT INTO sample_db.jobs (id, job) VALUES(%s, %s);",parameters)
             self.conn.commit()
+            return {"success": True, "message":"{0} Rows Sucessfully Added".format(len(parameters))}
         except Exception as e:
             self.conn.rollback()
+            return {"success": False, "message":e}
 
     def insert_departments(self,parameters):
         try:
             self.cursor.executemany("INSERT INTO sample_db.departments (id, department_name) VALUES(%s, %s);",parameters)
             self.conn.commit()
+            return {"success": True, "message":"{0} Rows Sucessfully Added".format(len(parameters))}
         except Exception as e:
             self.conn.rollback()
+            return {"success": False, "message":e}
 
     def insert_employees(self,parameters):
         try:
             self.cursor.executemany("INSERT INTO sample_db.hired_employees (id, name, hired_at, department_id, job_id) VALUES(%s, %s, %s, %s, %s);",parameters)
             self.conn.commit()
+            return {"success": True, "message":"{0} Rows Sucessfully Added".format(len(parameters))}
         except Exception as e:
             self.conn.rollback()
+            return {"success": False, "message":e}
